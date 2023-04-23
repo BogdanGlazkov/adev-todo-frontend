8f1e3a93eb4355c513b583bba9183258884f67f8
BogdanGlazkov <bglazkov@i.ua>
2023-04-23 21:51:23 +0300

    Fix form submit

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,34 +1,51 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 type Props = {
-  saveTodo: (e: React.FormEvent, formData: ITodo | any) => void
-}
+  saveTodo: (e: React.FormEvent, formData: ITodo | any) => void;
+};
 
-const AddTodo: React.FC<Props> = ({saveTodo}) => {
-  const [formData, setFormData] = useState<ITodo | {}>()
+const AddTodo: React.FC<Props> = ({ saveTodo }) => {
+  const [title, setTitle] = useState("");
+  const [descr, setDescr] = useState("");
+  const [formData, setFormData] = useState<ITodo | {}>();
 
   const handleForm = (e: React.FormEvent<HTMLInputElement>): void => {
-    setFormData({
-      ...formData, [e.currentTarget.id]: e.currentTarget.value,
-    })
-  }
+    e.currentTarget.id === "name"
+      ? setTitle(e.currentTarget.value)
+      : setDescr(e.currentTarget.value);
 
+    setFormData({
+      ...formData,
+      [e.currentTarget.id]: e.currentTarget.value,
+    });
+  };
+
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    saveTodo(e, formData);
+    setTitle("");
+    setDescr("");
+  };
 
   return (
-    <form className="Form" onSubmit={(e) => saveTodo(e, formData)}>
+    <form className="Form" onSubmit={onSubmit}>
       <div>
         <div>
           <label htmlFor="name">Name</label>
-          <input onChange={handleForm} type='text' id='name' />
+          <input onChange={handleForm} type="text" id="name" value={title} />
         </div>
         <div>
           <label htmlFor="description">Description</label>
-          <input onChange={handleForm} type='text' id='description' />
+          <input
+            onChange={handleForm}
+            type="text"
+            id="description"
+            value={descr}
+          />
         </div>
       </div>
       <button disabled={formData === undefined ? true : false}>Add Todo</button>
-
     </form>
-  )
-}
-export default AddTodo
+  );
+};
+export default AddTodo;
