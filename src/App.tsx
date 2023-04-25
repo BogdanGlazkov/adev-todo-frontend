7b8bcacbecb4f6955ab8855d381df7375199131b
BogdanGlazkov <bglazkov@i.ua>
2023-04-25 23:02:34 +0300

    Fix app routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,27 @@
-import React, {useState, useEffect} from 'react'
-import TodoItem from './components/TodoItem'
-import AddTodo from './components/AddTodo'
-import {getTodos, addTodo, updateTodo, deleteTodo} from './API'
+import { useState, useEffect } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import RegistrationPage from "./pages/RegistrationPage";
+import TodoPage from "./pages/TodoPage";
 
-
-function App() {
-  const [todos, setTodos] = useState<ITodo[]>([])
+const App = () => {
+  const [isAuth, setIsAuth] = useState(false);
+  const token = localStorage.getItem("token");
 
   useEffect(() => {
-    fetchTodos()
-
-  }, [])
-
-  const fetchTodos = (): void => {
-    getTodos()
-      .then(({data: {todos}}: ITodo[] | any) => setTodos(todos))
-      .catch((err: Error) => console.log(err))
-  }
-
-  const handleSaveTodo = (e: React.FormEvent, formData: ITodo): void => {
-    e.preventDefault()
-    addTodo(formData)
-      .then(({status, data}) => {
-        if (status !== 201) {
-          throw new Error('Error! Todo not saved')
-        }
-        setTodos(data.todos)
-      })
-      .catch(err => console.log(err))
-  }
-
-  const handleUpdateTodo = (todo: ITodo): void => {
-    updateTodo(todo)
-      .then(({status, data}) => {
-        if (status !== 200) {
-          throw new Error("Error! Todo not updated")
-        }
-        setTodos(data.todos)
-      })
-      .catch(err => console.log(err))
-  }
-
-  const handleDeleteTodo = (_id: string): void => {
-    deleteTodo(_id)
-      .then(({status, data}) => {
-        if (status != 200) {
-          throw new Error('Error! Todo not updated')
-        }
-        setTodos(data.todos)
-      })
-      .catch(err => console.log(err))
-  }
+    setIsAuth(!!token);
+  }, [token]);
 
   return (
-    <main className="App">
-      <h1>My Todos</h1>
-      <AddTodo  saveTodo={handleSaveTodo}/>
-      { 
-        todos.map((todo:ITodo) => (
-          <TodoItem 
-          key={todo._id}
-          updateTodo={handleUpdateTodo}
-          deleteTodo={handleDeleteTodo}
-          todo={todo}
-          />
-        ))
-      }
-    </main>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegistrationPage />} />
+        <Route path="/todos" element={<TodoPage />} />
+        <Route path="*" element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-}
-
-export default App
+export default App;
